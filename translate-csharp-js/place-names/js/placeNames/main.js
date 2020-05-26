8ec8b8ae3f341188d693b10a0195d00f605dec0b
Drew Kirkland <drew.kirkland@gmail.com>
2020-05-26 14:21:57 -0400

    PlaceNames complete.

--- translate-csharp-js/place-names/js/placeNames/main.js
+++ translate-csharp-js/place-names/js/placeNames/main.js
@@ -1,6 +1,27 @@
+const placesArray = [
+  "Nashville",
+  "Hong Kong",
+  "The back yard",
+  "Earth",
+  "London",
+  "The mall",
+  "Ryman Auditorium",
+  "The Great Wall of China",
+];
+const thePlaces = placesArray.filter((place) => {
+  return place.includes("The");
+});
+
 function main() {
   // Put your code here
 
-}
+  // Print some output to the terminal (aka. "Console");
+  console.log("These are definitely places");
 
+  placesArray.map((place) => console.log(place));
+
+  console.log("These are definitely THE places");
+
+  thePlaces.map((thePlace) => console.log(thePlace));
+}
 main();
