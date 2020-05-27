8fda036180882dbaaf0d767d4125623c6c58c350
Drew Kirkland <drew.kirkland@gmail.com>
2020-05-27 15:23:45 -0400

    Setup three buildings.

--- planner/Planner/Program.cs
+++ planner/Planner/Program.cs
@@ -6,7 +6,37 @@ namespace Planner
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Building OneMain = new Building("1 Main St.")
+            {
+                Stories = 10,
+                Width = 100,
+                Depth = 100,
+            };
+
+            OneMain.Construct();
+            OneMain.Purchase("Adam Richguy");
+
+            Building TwoMain = new Building("2 Main St.")
+            {
+                Stories = 20,
+                Width = 200,
+                Depth = 200,
+            };
+
+            TwoMain.Construct();
+            TwoMain.Purchase("Mo Lotsamoney");
+
+            Building ThreeMain = new Building("3 Main St.")
+            {
+                Stories = 30,
+                Width = 300,
+                Depth = 300,
+            };
+
+            ThreeMain.Construct();
+            ThreeMain.Purchase("Andy Megawealthy");
+            
+
         }
     }
 }
