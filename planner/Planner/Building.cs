af0ede97c1f41f6c317550392015e7d5745ccbed
Drew Kirkland <drew.kirkland@gmail.com>
2020-05-27 15:10:07 -0400

    Setup Building class for Urban Planner exercise.

--- planner/Planner/Building.cs
+++ planner/Planner/Building.cs
@@ -1,12 +1,38 @@
+using System;
+
 namespace Planner
 {
     class Building
     {
+        private string _designer;
+        private DateTime _dateConstructed;
+        private string _address;
+        private string _owner;
+
         public int Stories { get; set; }
-        public int Width { get; set; }
-        public int Depth { get; set; }
-        public int Volume { get{
-            return 
-        }}
+        public double Width { get; set; }
+        public double Depth { get; set; }
+        public double Volume
+        {
+            get
+            {
+                return Width * Depth * (Stories * 3);
+            }
+        }
+
+        public void Construct()
+        {
+            _dateConstructed = DateTime.Now;
+        }
+        public void Purchase(string owner)
+        {
+            _owner = owner;
+        }
+        //Constructor Method
+        public Building(string address)
+        {
+            _address = address;
+
+        }
     }
 }
